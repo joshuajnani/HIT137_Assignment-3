081aea3a3edf75dd876f025753b653ca9aa4b025
salmonoid06 <108784019+salmonoid06@users.noreply.github.com>
2025-10-02 01:41:11 +0700

    Add files via upload

--- background_remover.py
+++ background_remover.py
@@ -1,4 +1,8 @@
-# Use a pipeline as a high-level helper
+# background_remover.py
+
 from transformers import pipeline
 
-pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True, use_fast=True, device=0)
+def load_background_remover():
+    # Load the model for background removal
+    return pipeline(
+        "image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
